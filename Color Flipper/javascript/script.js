27965b4610eb91ad808d663488a7ff6bcdace693
Ulugbek Akhmedov <121518041+firecoderr@users.noreply.github.com>
2023-10-16 01:26:06 +0500

    Add files via upload

---
+++ Color Flipper/javascript/script.js
@@ -0,0 +1,18 @@
+const colors = ["red", "orange", "blue", "pink", "green", "yellow", "grey", "white", "black"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+console.log(color);
+console.log(btn)
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
